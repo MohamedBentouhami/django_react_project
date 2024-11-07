c70b3e0115413ec4ae31f2ed81e5166def5da21c
Mohamed <mohamedbentouhami512@gmail.com>
2024-11-07 11:57:59 +0100

    add generics views related the note

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,4 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Note(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.TimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__ (self): 
+        return self.title
